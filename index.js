1f6124050103dfc1718b9e21b86c056b0d251eb9
sooraj tgc <sooraj.tgc@gmail.com>
2024-02-11 12:53:26 +0530

    error handling, 404 path

--- index.js
+++ index.js
@@ -4,23 +4,37 @@ const app = express();
 const PORT = 8000;
 app.use(express.json());
 
-app.get("/base-path", (req, res) => {
+app.get("/api", (req, res) => {
   res.send({
     success: true,
     data: [{ name: "temp" }],
   });
 });
 
-app.post("/base-path", (req, res) => {
+app.post("/api", (req, res) => {
   try {
     const { name } = req.body;
+
+    if (!name) throw new Error("name is not present in payload");
+
     res.send({
       success: true,
-      data: [{ name: name }],
+      data: { name: name },
     });
   } catch (error) {
     console.log(error.message);
+    res.send({
+      success: false,
+      error: error.message,
+    });
   }
+});
+
+app.use("/*", (req, res) => {
+  res.send({
+    message: "server is running",
+    baseUrl: "http://localhost:8000/api",
+  });
 });
 
 try {
